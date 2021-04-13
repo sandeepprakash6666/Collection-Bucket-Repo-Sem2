b407ecaa3934d3f124e0c39a8e6c610211ec2ca7
sandeepprakash6666 <sandeepprakash6666@gmail.com>
2021-04-13 16:36:51 +0200

    added plots wsl package test

---
+++ Cao_Battery paper code/Untitled.jl
@@ -0,0 +1,34 @@
+
+
+
+using Plots
+plotly()
+
+a = 100*rand(100)
+
+plot(a,a)
+
+
+
+
+
+##*
+using PlotlyJS
+
+a = 100*rand(100)
+
+plot(a, a)
+
+
+plt = PlotlyJS.scatter(; x = a, y = a)
+
+PlotlyJS.plot([plt])
+
+
+function linescatter1()
+    trace1 = scatter(;x=1:4, y=[10, 15, 13, 17], mode="markers")
+    trace2 = scatter(;x=2:5, y=[16, 5, 11, 9], mode="lines")
+    trace3 = scatter(;x=1:4, y=[12, 9, 15, 12], mode="lines+markers")
+    plot([trace1, trace2, trace3])
+end
+linescatter1()
